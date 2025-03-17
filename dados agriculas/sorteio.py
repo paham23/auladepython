176b0ba912895ca5535f8f7458a9315bab309b39
paham23 <paulacursos6@gmail.com>
2025-03-17 19:49:50 -0400

    Add files via upload

---
+++ dados agriculas/sorteio.py
@@ -0,0 +1,21 @@
+import random
+def sorteio_aluno():
+    alunos = [
+        "Jose", "ana", "pedro",
+        "julia", "maria", "fabiana"
+    ]
+    escolher = random. choice(alunos)
+    print(f"o aluno escolhido foi: {escolher}")
+sorteio_aluno()
+
+#sorteio com numeros 
+
+import random 
+def sorteio_numeros():
+    numeros = [
+        "56", "74", "45",
+        "25", "68", "80"
+    ]
+    escolher = random. choice(numeros)
+    print(f"o numero escolhido foi: {escolher}")
+sorteio_numeros()
